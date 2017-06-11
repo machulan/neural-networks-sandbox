c7acaf417c82f8624c410cd5a9c396ada7923a08
Nik <nik.machula@yandex.ru>
2017-06-12 01:33:19 +0400

    Improved project structure.

--- image_handler.py
+++ image_handler.py
@@ -1,6 +1,28 @@
 from PIL import Image
 
 
+def print_image_data1(image):
+    print('Image data:')
+    print('format:', image.format)
+    print('mode:', image.mode)
+    print('width:', image.width)
+    print('height:', image.height)
+
+
+def print_image_data(image):
+    print('IMAGE DATA:')
+    print('format:', image.format, 'mode:', image.mode)
+    print('width:', image.width, 'height:', image.height)
+
+
 def handle_image(image):
     print('image handling running...')
-    return Image.open('../neural-networks-sandbox/images/doctorwho.jpg')
+
+    print_image_data(image)
+
+
+    stub = Image.open('../neural-networks-sandbox/images/doctorwho.jpg')
+
+    stub = image.resize((image.width * 2, image.height * 2))
+
+    return stub
